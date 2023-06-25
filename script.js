e7481a6d50be14a458664bccedbeb020ce76cc6b
FelipeSoft <felipedecastrolima2@gmail.com>
2023-06-25 19:12:44 -0300

    Limpando as informações do tabuleiro

--- script.js
+++ script.js
@@ -13,57 +13,84 @@ var points_x = 0;
 var points_o = 0;
 
 reset();
+c('div.actions button#restart').addEventListener('click', reset);
+c('div.actions button#play-again').addEventListener('click', clear);
 cs('div.item').forEach(element => element.addEventListener('click', render));
 
 function render(e){
-    var selectedObject = e.target;
-    if(gameBoard[selectedObject.getAttribute('position')] === ""){
-        if(turn === 'X'){
-            gameBoard[selectedObject.getAttribute('position')] = 'X';
-            c(`div.item[position*=${selectedObject.getAttribute('position')}]`).innerHTML = "<div class='x'><div class='stick1'></div><div class='stick2'></div></div>";
-            turn = 'O';
-            check('X');
-        } else if(turn === 'O'){
-            gameBoard[selectedObject.getAttribute('position')] = 'O';
-            c(`div.item[position*=${selectedObject.getAttribute('position')}]`).innerHTML = "<div class='circle'></div>";
-            turn = 'X';
-            check('O');
+    if(statusPlaying === true){
+        warning = 'Partida em andamento...';
+        var selectedObject = e.target;
+        if(gameBoard[selectedObject.getAttribute('position')] === ""){
+            if(turn === 'X'){
+                gameBoard[selectedObject.getAttribute('position')] = 'X';
+                c(`div.item[position*=${selectedObject.getAttribute('position')}]`).innerHTML = "<div class='x'><div class='stick1'></div><div class='stick2'></div></div>";
+                turn = 'O';
+                check('X');
+            } else if(turn === 'O'){
+                gameBoard[selectedObject.getAttribute('position')] = 'O';
+                c(`div.item[position*=${selectedObject.getAttribute('position')}]`).innerHTML = "<div class='circle'></div>";
+                turn = 'X';
+                check('O');
+            }
         }
+        setTimeout(()=>{
+            cs("div.circle, div.x").forEach(element => {
+                element.style.bottom = "0px";
+                element.style.opacity = 1;
+            });
+        }, 0);
+        current();
+        c('div.warning').innerHTML = warning;
+        c('p.points-x').innerHTML = points_x;
+        c('p.points-o').innerHTML = points_o;
     }
-    setTimeout(()=>{
-        cs("div.circle, div.x").forEach(element => {
-            element.style.bottom = "0px";
-            element.style.opacity = 1;
-        });
-    }, 0);
-    current();
-    c('div.warning').innerHTML = warning;
 }
 
 function reset(){
-    warning = "Começar o jogo ou selecionar jogador";
-    var gameBoard = {
-        a1: '', a2: '', a3: '',
-        b1: '', b2: '', b3: '',
-        c1: '', c2: '', c3: ''
+    for(let i in gameBoard){
+        gameBoard[i] = '';
     }
+    warning = "Começar o jogo ou selecionar jogador.";
+    var points_x = 0;
+    var points_o = 0;
+    c('p.points-x').innerHTML = points_x;
+    c('p.points-o').innerHTML = points_o;
+    c('div.actions button#restart').style.display = 'none';
+    c('div.actions button#play-again').style.display = 'none';
 }
 
-function warning(){
-
+function clear(){
+    for(let i in gameBoard){
+        gameBoard[i] = '';
+    }
+    cs('div.container div.item div.circle, div.container div.item div.x').forEach(element => {
+        element.remove();
+    });
+    warning = "Partida em andamento...";
+    statusPlaying = true;
+    c('div.warning').innerHTML = warning;
+    c('div.actions button#play-again').style.display = "none";
+    c('div.actions button#restart').style.display = "none";
 }
 
 function current(){
     if(check('X')){
         points_x++;
-        c('div.points-x').innerHTML = points_x;
         warning = "O jogador X venceu!";
+        statusPlaying = false;
+        c('div.actions button#play-again').style.display = "block";
+        c('div.actions button#restart').style.display = "block";
     } else if(check('O')){
         points_o++;
-        c('div.points-o').innerHTML = points_o;
         warning = "O jogador O venceu!";
+        statusPlaying = false;
+        c('div.actions button#play-again').style.display = "block";
+        c('div.actions button#restart').style.display = "block";
     } else if(draw()){
         warning = "Empate!";
+        c('div.actions button#play-again').style.display = "block";
+        c('div.actions button#restart').style.display = "block";
     }
 }
 
