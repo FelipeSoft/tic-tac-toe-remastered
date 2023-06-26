a471d440718c256bf543d1925f4a7f945fc41686
FelipeSoft <felipedecastrolima2@gmail.com>
2023-06-26 14:47:17 -0300

    Otimização da verificação de vitória e implementação de novas funcionalidades do layout

--- script.js
+++ script.js
@@ -12,24 +12,35 @@ var gameBoard = {
 var points_x = 0;
 var points_o = 0;
 
-reset();
-c('div.actions button#restart').addEventListener('click', reset);
-c('div.actions button#play-again').addEventListener('click', clear);
+resetWithRestrictions(true);
+c('div.score-item:nth-child(1)').addEventListener('click', () => {
+    if(statusPlaying === false) turn = "X";
+});
+c('div.score-item:nth-child(2)').addEventListener('click', () => {
+    if(statusPlaying === false) turn = "O";
+});
+c('div.actions button#restart').addEventListener('click', () => {
+    resetWithRestrictions(true);
+});
+c('div.actions button#play-again').addEventListener('click', () => {
+    resetWithRestrictions(false);
+});
 cs('div.item').forEach(element => element.addEventListener('click', render));
 
 function render(e){
     if(statusPlaying === true){
-        warning = 'Partida em andamento...';
+        warning = 'Match in progress...';
         var selectedObject = e.target;
-        if(gameBoard[selectedObject.getAttribute('position')] === ""){
+        var position = selectedObject.getAttribute('position');
+        if(gameBoard[position] === ""){
             if(turn === 'X'){
-                gameBoard[selectedObject.getAttribute('position')] = 'X';
-                c(`div.item[position*=${selectedObject.getAttribute('position')}]`).innerHTML = "<div class='x'><div class='stick1'></div><div class='stick2'></div></div>";
+                gameBoard[position] = 'X';
+                c(`div.item[position*=${position}]`).innerHTML = "<div class='x'><div class='stick1'></div><div class='stick2'></div></div>";
                 turn = 'O';
                 check('X');
             } else if(turn === 'O'){
-                gameBoard[selectedObject.getAttribute('position')] = 'O';
-                c(`div.item[position*=${selectedObject.getAttribute('position')}]`).innerHTML = "<div class='circle'></div>";
+                gameBoard[position] = 'O';
+                c(`div.item[position*=${position}]`).innerHTML = "<div class='circle'></div>";
                 turn = 'X';
                 check('O');
             }
@@ -47,31 +58,28 @@ function render(e){
     }
 }
 
-function reset(){
-    for(let i in gameBoard){
-        gameBoard[i] = '';
-    }
-    warning = "Começar o jogo ou selecionar jogador.";
-    var points_x = 0;
-    var points_o = 0;
-    c('p.points-x').innerHTML = points_x;
-    c('p.points-o').innerHTML = points_o;
-    c('div.actions button#restart').style.display = 'none';
-    c('div.actions button#play-again').style.display = 'none';
-}
-
-function clear(){
+function resetWithRestrictions(constraint){
     for(let i in gameBoard){
         gameBoard[i] = '';
     }
     cs('div.container div.item div.circle, div.container div.item div.x').forEach(element => {
         element.remove();
     });
-    warning = "Partida em andamento...";
-    statusPlaying = true;
+
+    if(constraint === true){
+        statusPlaying = true;
+        warning = "Start the game or choose a player.";
+        points_x = points_o = 0;
+    } else if(constraint === false){
+        warning = "Match in progress...";
+        statusPlaying = true;
+    }
+    
+    c('p.points-x').innerHTML = points_x;
+    c('p.points-o').innerHTML = points_o;
+    c('div.actions button#restart').style.display = 'none';
+    c('div.actions button#play-again').style.display = 'none';
     c('div.warning').innerHTML = warning;
-    c('div.actions button#play-again').style.display = "none";
-    c('div.actions button#restart').style.display = "none";
 }
 
 function current(){
@@ -94,22 +102,32 @@ function current(){
     }
 }
 
-function check(player){
-    let possibilities = [
-        'a1,b1,c1',
-        'a2,b2,c2',
-        'a3,b3,c3',
-        'a1,a2,a3',
-        'b1,b2,b3',
-        'c1,c2,c3',
-        'a1,b2,c3',
-        'c1,b2,a3'
+function check(player) {
+    const possibilities = [
+        ['a1', 'b1', 'c1'],
+        ['a2', 'b2', 'c2'],
+        ['a3', 'b3', 'c3'],
+        ['a1', 'a2', 'a3'],
+        ['b1', 'b2', 'b3'],
+        ['c1', 'c2', 'c3'],
+        ['a1', 'b2', 'c3'],
+        ['c1', 'b2', 'a3']
     ];
 
-    for(let position in possibilities){
-        let match = possibilities[position].split(",")
-        let hasWon = match.every(element => gameBoard[element] === player);
-        if(hasWon) return true;
+    for (let i = 0; i < possibilities.length; i++) {
+        const match = possibilities[i];
+        let hasWon = true;
+
+        for (let j = 0; j < match.length; j++) {
+            if (gameBoard[match[j]] !== player) {
+                hasWon = false;
+                break;
+            }
+        }
+
+        if (hasWon) {
+            return true;
+        }
     }
 
     return false;
